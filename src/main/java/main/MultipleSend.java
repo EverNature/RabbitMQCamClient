136d0220fa72e726616a4bc1181022758cb552db
GDLP8 <jorge.dasilva@alumni.mondragon.edu>
2022-06-05 16:39:37 +0200

    ensure cameras store not sent photos

--- src/main/java/main/MultipleSend.java
+++ src/main/java/main/MultipleSend.java
@@ -1,14 +1,19 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.ConnectException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +26,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+
+import javax.imageio.ImageIO;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,8 +48,11 @@ public class MultipleSend {
 	final static String DLX_QUEUE_NAME = "deadLetterQueue";
 	
     final static String PHOTOS_FOLDER = "photos";
+    final static String TO_SEND = "toSend";
     final static Integer MAX_WAIT = 9;
     boolean stop;
+    boolean connected;
+    boolean first;
     
     Random random;
     List<Path> lista;
@@ -54,8 +64,9 @@ public class MultipleSend {
     	random = new Random();
     	lista = getFileNames(PHOTOS_FOLDER);
     	stop = false;
+    	connected = false;
+    	first = true;
     	executor = Executors.newCachedThreadPool();
-    	
     }
     
     public List<Path> getFileNames(String folder){
@@ -85,48 +96,65 @@ public class MultipleSend {
             uuid= Integer.parseInt(prop.getProperty("uuid"));
 			input.close();
 			
-			Connection connection;
-			connection = connectionFactory.newConnection();
-			Channel channel = connection.createChannel();
+			Connection connection = null;
+			Channel channel = null;
+			String tag = null;
+			
+			while(!stop) {
+				if(!connected)
+				{
+					try {
+						connection = connectionFactory.newConnection();
+						channel = connection.createChannel();
 
-	        channel.exchangeDeclare(EXCHANGE_CAMERAS, "fanout");
-        	channel.exchangeDeclare(DLX_EXCHANGE_NAME, "fanout");
-			
-			channel.queueDeclare(DLX_QUEUE_NAME, false, false, true, null);
-			channel.queueBind(DLX_QUEUE_NAME,DLX_EXCHANGE_NAME,"");
-			
-			boolean autoack = true;
-            
-			ConsumerClient consumer = new ConsumerClient(channel, executor);
-			String tag = channel.basicConsume(DLX_QUEUE_NAME, autoack, consumer);
-	        
-	        while(!stop) {
+				        channel.exchangeDeclare(EXCHANGE_CAMERAS, "fanout", true, false, false, null);
+			        	channel.exchangeDeclare(DLX_EXCHANGE_NAME, "fanout", true, false, false, null);
+						
+						channel.queueDeclare(DLX_QUEUE_NAME, true, false, true, null);
+						channel.queueBind(DLX_QUEUE_NAME,DLX_EXCHANGE_NAME,"");
+						
+						boolean autoack = true;
+			            
+						ConsumerClient consumer = new ConsumerClient(channel, executor);
+						tag = channel.basicConsume(DLX_QUEUE_NAME, autoack, consumer);
+						connected = true;
+					} catch (ConnectException e) {
+						System.out.println("z");
+					}
+				}
+				
+				if(connected && first) {
+					List<Path> list = getFileNames(TO_SEND);
+					for(Path p:list)
+					{
+						byte[] fileContent = Files.readAllBytes(p);
+						sendPhoto(fileContent, p, channel);
+						p.toFile().delete();
+					}
+				}
+				
 	        	Thread.sleep(random.nextInt(MAX_WAIT)*1000);
 	        	Path path = lista.get(random.nextInt(lista.size()));
 	        	byte[] fileContent = Files.readAllBytes(path);
-	            byte[] message = Base64.getEncoder().encode(fileContent);
-	            
-	            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-	            messageDigest.update(message);
-	            
-	            String filename = path.getFileName().toString();
-	            
-	            AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties().builder();
-	            
-				Map<String,Object> headerMap = new HashMap<String, Object>();
-				headerMap.put("hash", new String(messageDigest.digest()));
-				headerMap.put("uuid", uuid.toString());
-				headerMap.put("filename", filename.substring(0, filename.lastIndexOf(".")));
-				builder.headers(headerMap);
-	            
-	            channel.basicPublish(EXCHANGE_CAMERAS, "", builder.build(), message);
-//	            channel.basicPublish(EXCHANGE_CAMERAS, "", builder.build(), "hola".getBytes());
-
-	            LOGGER.info(" [x] Sent '" + path + "'");
+	        	
+	        	try {
+	        		if(connection.isOpen()) {
+	        			sendPhoto(fileContent, path, channel);
+		            }
+				} catch (NullPointerException e) {
+					InputStream is = new ByteArrayInputStream(fileContent);
+	            	BufferedImage bi = ImageIO.read(is);
+                	File file = new File(FileSystems.getDefault().getPath(TO_SEND, (new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new java.util.Date()) + ".jpg")).toString());
+                    ImageIO.write(bi, "jpg", file);
+                    is.close();
+				}
 	        }
-	        channel.basicCancel(tag);
-	        channel.close();
-	        connection.close();
+	        if(channel != null && connection != null)
+	        {
+	        	channel.basicCancel(tag);
+		        channel.close();
+		        connection.close();
+	        }
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (TimeoutException e) {
@@ -134,13 +162,33 @@ public class MultipleSend {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     }
     
 	public synchronized void stop() {
 		stop = true;
+	}
+	
+	public void sendPhoto(byte[] fileContent, Path path, Channel channel) throws NoSuchAlgorithmException, IOException {
+		byte[] message = Base64.getEncoder().encode(fileContent);
+    	MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(message);
+        
+        String filename = path.getFileName().toString();
+        
+        AMQP.BasicProperties.Builder builder = new AMQP.BasicProperties().builder();
+        
+		Map<String,Object> headerMap = new HashMap<String, Object>();
+		headerMap.put("hash", new String(messageDigest.digest()));
+		headerMap.put("camera_id", uuid.toString());
+		headerMap.put("filename", filename.substring(0, filename.lastIndexOf(".")));
+		builder.headers(headerMap);
+		builder.deliveryMode(2);
+        
+        channel.basicPublish(EXCHANGE_CAMERAS, "", builder.build(), message);
+
+        LOGGER.info(" [x] Sent '" + path + "'");
 	}
 	
 	public class ConsumerClient extends DefaultConsumer {
@@ -162,7 +210,11 @@ public class MultipleSend {
 			try {
                 executor.submit(new Runnable() {
                     public void run() {
-                    	LOGGER.warn(String.format("No leido. Duro"));
+                    	try {
+							channel.basicPublish(EXCHANGE_CAMERAS, "", properties, message.getBytes());
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
                     }
                 });
             } catch (Exception e) {
@@ -171,7 +223,11 @@ public class MultipleSend {
 		}
     }
 
-    public static void main(String[] args) throws IOException {
+    public boolean isStop() {
+		return stop;
+	}
+
+	public static void main(String[] args) throws IOException {
     	Scanner scanner = new Scanner(System.in);
         MultipleSend ms = new MultipleSend();
         Thread waitThread = new Thread(()-> {
@@ -179,7 +235,9 @@ public class MultipleSend {
 			ms.stop();
 		});
         waitThread.start();
-        ms.register();
+        while(!ms.isStop()) {
+        	ms.register();
+        }
         scanner.close();
     }
 }
